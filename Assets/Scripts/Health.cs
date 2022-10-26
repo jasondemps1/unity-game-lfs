bd0e8f958df7e48e81f4a01a7b01e76dbe6a82a4
Jason Dempsey <jasondemps1@gmail.com>
2022-10-26 08:34:01 -0400

    Added Health and Test level

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public class Health : MonoBehaviour
+    {
+        public int Value;
+
+        public void Awake()
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
